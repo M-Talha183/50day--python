9afbf87f923835ee94ed1d7b80e90458b974112c
Muhammad361738 <ullahehsan016@gmail.com>
2024-11-29 00:36:57 +0500

    Add new task in python journey

--- day04/task10.py
+++ day04/task10.py
@@ -5,24 +5,23 @@
 #  'good night'to the user
 # when a user enters the time 
 # in hours:minutes:seconds """
-# '02:00:01'
-print("hello world ")
-wishingTime = str(input('Enter the Time in Hours minute and sec make sure we are in 24 Hours system "00 : 00 : 00" '))
-if(wishingTime > "24 : 00 : 00"):
-    print("please enter valid time  ")
-elif(wishingTime < "04:00:00" ):
-    print("Good Night")
-elif(wishingTime >= "05:00:00" and wishingTime < "12:00:00"):
-    print("Good Morning")
-elif(wishingTime >= "12:00:00" and wishingTime < "13:00:00"):
+# '02:00:01'                              
+
+wishingTime = (input('Enter the Timestamp "0h : 0m : 0s : " '))
+if(wishingTime >="05:00:00" and wishingTime <="11:59:59" ):
+    print("Good Morning ")
+elif(wishingTime >= "12:00:00" and wishingTime <="14:59:59" ):
     print("Good Noon")
-elif(wishingTime >= "13:00:00" and wishingTime < "17:00:00"):
+elif(wishingTime >= "15:00:00" and wishingTime <= "16:59:59"):
     print("Good Afternoon")
-elif(wishingTime >= "17:00:00" and wishingTime < "20:00:00"):
+elif(wishingTime > "17:00:00" and wishingTime <= "19:59:59"):
     print("Good Evening")
-# elif(wishingTime >= "20:00:00" and wishingTime <= "24:00:00"):
-#     print("Good Night")
+elif(wishingTime >= "20:00:00" and wishingTime < "23:59:59"):
+    print("Good Night")
+elif(wishingTime >= "00:00:00" and wishingTime < "04:59:59"):
+    print("Good Late night")
+  
 else:
-    print("good night")
+    print("Enter valid Time ")
 
 
--- day05-Disct/disctionary_basic.py
+++ day05-Disct/disctionary_basic.py
@@ -1,22 +1,40 @@
-# disctionary is the data strucure
-my_dist = {"pakistan":'karachi',"USA": 'New York', "France":'london'}
-# key + value = pair 
-print(my_dist)
-print(len(my_dist))
-# Accesing value via keys
-print(my_dist['pakistan']) 
-print(my_dist['USA']) 
-print(my_dist['France'])
-#  using accessing Only keys  
-print(my_dist.keys())
-#  accessing values 
-print(my_dist.values())
-# print(my_dist["talha"]) # key erroe if key is not present 
-print(my_dist.get('usa')) # if you want to check your kay and in reason of 
-# absent the key  you dont want to get error so use get () method 
-print(my_dist.get("USA"))
+# # disctionary is the data strucure
+# my_dist = {"pakistan":'karachi',"USA": 'New York', "France":'london'}
+# # # key + value = pair 
+# # print(type(my_dist))
+# # # print(type(my_dist)) checking data type
+# # print(my_dist)
+# # print(len(my_dist))
+# # #  checking is typecasting is possible in dictionary
+# # # print(int(my_dist)) intiger
+# # my_dict2 = {'2':'1', '4':'6'}
+# # print(int(my_dict2))
+# # #  type casting dict to str 
 
-# my_list = []
-# my_list [10] = 2
-my_dist ["10"] = "2"
-print(my_dist)
+
+# #  type casting dictionary to list 
+
+# # my_list = [1,2,3,4] 
+# # print(dict(my_list))
+# # methods od dictionary 
+# print(my_dist.keys())
+# print(my_dist.values())
+# print(my_dist.items())
+# print(my_dist.get("usa"))
+# # print(my_dist["india"])
+# print(my_dist.pop("pakistan"))
+# print(my_dist.popitem())
+# employ_data = {"talha":10, "zain":15}
+# employ_data.clear()
+# print(employ_data)
+# employ_data = {"pakistan","zindabad"}
+# print(employ_data)
+# employ_data ['myName'] = "talha"
+# print(employ_data)
+# my_dist = {"iran":"thehran","uae":"dubai"}
+# my_dist.update()
+# print(my_dist)
+
+#  write a prrogram taht takes a list of multiple choice response 
+# Eg: [a,b,c] and prints a dictionary of question - response pairs
+# {'q':"A", q2: "B","q3":"c"}:
