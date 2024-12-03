1e580a49cd2349fce190153c89878309d80be657
Muhammad361738 <ullahehsan016@gmail.com>
2024-12-03 14:15:22 +0500

    add new tasks in loops in python journey

--- day08_while_lopps/while_loop.py
+++ day08_while_lopps/while_loop.py
@@ -1,5 +1,37 @@
-fruits1 = ["apple","orange","cherry",]
-fruits2 = ["chery","graps","orange",]
-for i in fruits1 :
-    for j in fruits2:
-        print(i,j)
+# fruits1 = ["apple","orange","cherry",]
+# fruits2 = ["chery","graps","orange",]
+# for i in fruits1 :
+#     for j in fruits2:
+#         print(i,j)
+# 
+# 
+#  while looop 
+# i = 1 
+# while i<10:
+#     print(i)
+#     i+=2
+
+# i = 0
+# while i < 6:
+#     print(i)
+#     i+=1
+#     if i == 3:
+#      break
+# else:
+#    print("check is else is longer with else")
+
+#  continue statement check
+# i = 0
+# while i < 6:
+#     print(i)
+#     i+=1
+#     if i == 3:
+#      continue
+# else:
+#    print("check is continue  else is longer with else"
+# Task 1 : find the length of the string without using len function 
+str = "talha"
+i=0
+while i < len(str):
+    i+=1
+print(i)
