d0ce952e15df521f8a955499bcb91477e2f87913
Muhammad361738 <ullahehsan016@gmail.com>
2024-12-04 23:47:35 +0500

    Add new file of loops tasks realted with math number & logic

--- day08_while_lopps/while_loop.py
+++ day08_while_lopps/while_loop.py
@@ -30,8 +30,45 @@
 # else:
 #    print("check is continue  else is longer with else"
 # Task 1 : find the length of the string without using len function 
-str = "talha"
-i=0
-while i < len(str):
-    i+=1
-print(i)
+# str = "talha"
+# i=0
+# while i < len(str):
+#     i+=1
+# print(i)
+
+
+# num1 = 1500
+
+# count = 0
+# while (num1 <=2700):
+#      num1 +=1
+#      if(num1 % 7 ==0 and num1 %5 == 0 ):
+#           count +=1
+#           print(num1)
+
+# print(count)
+sum = 0
+li = [3,5,23,6,5,1,2,9,8]
+for i in li:
+    sum += i **2
+print(sum)
+    # sum += sqr
+    # print(sum)
+    # mysum = sum + num
+    # print(mysum)
+
+
+
+evenCount =0
+oddCount =0
+myseries = [1,2,3,4,5,6,7,8,9]
+for i in myseries:
+    if(i % 2 ==0):
+        evenCount += 1
+    else:
+        oddCount +=1
+print(evenCount)
+print(oddCount)
+
+
+
---
+++ loops_tasks/loops_tasks.py
@@ -0,0 +1,102 @@
+#  write the progrm to print the first 10 natural number 
+# print("for loop ")
+# for i in range(1,11):
+#     print(i)
+
+# num = 1
+# print("wHILE LOOP")
+# while num <= 10:
+#     print(num)
+#     num+=1
+#  write the progrm to print the first 10 whole  number 
+# print("for loop Whole number")
+# for i in range(0,10):
+#     print(i)
+
+# num = 0
+# print("wHILE LOOP whole number ")
+# while num < 10:
+#     print(num)
+#     num+=1
+
+# write the progrm to print the first 10 Even  number 
+# print("for loop Even Number  number")
+# for i in range(2,22,2):
+#     print(i)
+
+# num = 2
+# print("wHILE LOOP Even number ")
+# while num <= 20:
+#     print(num)
+#     num+=2
+# write the progrm to print the first 10 odd   number 
+# print("for loop Even Number  number")
+# for i in range(1,20,2):
+#     print(i)
+
+# num = 1
+# print("wHILE LOOP Even number ")
+# while num <= 20:
+#     print(num)
+#     num+=2
+
+# # Q : 3  write the program to print muliplition table of a given number 
+# myNum = int(input("Enter the number : "))
+# for i in range(1,11):
+#         print(f'{myNum} * {i} = {myNum*i}')
+
+# num = int(input("Enter the number :"))
+
+# myRange = int (input("Enter the range in which i give you prime number "))
+# if (myRange == 0 or myRange == 1):
+#     print("Not prime number ")
+# else:
+#     i = 2
+#     primeDectector = 0
+#     while (i < myRange):
+        
+#         if (myRange % i == 0 ):
+#             primeDectector = primeDectector +1
+            
+#         i+=1 
+
+
+# if primeDectector != 0 :
+#     print(f'this is {myRange} not  prime number')
+# else :
+
+#     print(f'this is {myRange} prime number')
+
+
+# myRange = int (input("Enter the range in which i give you prime number "))
+
+# for i in range(1,11):
+#     if(i == 0 or i ==1):
+#         continue
+#     else:
+#          a = 0
+#          primeDect = 0
+#          while(a<myRange):
+#              if(myRange % i ==0):
+#                  ("This is prime number ",i)
+#              a+=1
+#          else:
+#              print("this is not prime number ")
+                 
+                 
+number = [12,75,150,180,154,525,50]
+
+for i in number:
+    if(i % 5 ==0):
+        print(i)
+        if(i > 150):
+           continue
+           print("After greater then 150")
+           print(i)
+        if(i>500):
+              break
+              print("After greater then 500")
+              print(i)
+          
+             
+          
