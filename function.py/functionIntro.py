74e4ea3308229c3afc7050fd041a40c1da544393
Muhammad361738 <ullahehsan016@gmail.com>
2024-12-05 14:55:44 +0500

    add new tasks in loops

---
+++ function.py/functionIntro.py
@@ -0,0 +1,21 @@
+#  uderstading the basic structure of function
+# def my_sum (a,b):
+#     addition = a+b
+#     return (addition)
+# # my_sum(2,3)
+
+
+# def gmean (a,b,c):
+#     myMean = a / b
+#     myMean2 = b/c
+#     if(myMean == myMean2):
+#         print(myMean)
+#     else:
+#         print("This is not geo matic series ")
+
+# def Gmean (a,b):
+#     '"This function calculates "'
+#     Gmean = a+b
+#     cemean = a*b
+#     return Gmean , cemean
+# Gmean(5,5)
