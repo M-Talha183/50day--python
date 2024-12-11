c23339013bc0048b645e0c2fbb075d7d17e20824
Muhammad361738 <ullahehsan016@gmail.com>
2024-12-12 00:33:56 +0500

    Add new file of case sensetive  in python joureny

---
+++ python_neduet/case_sensitivity.py
@@ -0,0 +1,32 @@
+#camel case in python
+pythonProgramming = 'Group2'
+#pascal case in python
+PythonProgramming = 'Group2'
+#Snake case
+_python_programming = 'Group2'
+AGE = 3
+age = 5
+Age = 6
+
+age_2 = 4
+age = 4
+
+'''Python is a case sensitive language.
+Following are the 'donts' while declaring a 
+variable
+1) Variable name must not be a python's keyword.
+For example: as, break, continue, for, in, and,
+or, try, finally, except etc..
+2) Vaiable name must not be starting with any
+number. For example: '2_my_variable' 
+is incorrect'''
+
+# 5myvariable = 4
+
+myvariable2 = 0
+my_vars= 33
+
+myName = "Talha"
+myName = 5
+print(myName)
+# print(myName)
