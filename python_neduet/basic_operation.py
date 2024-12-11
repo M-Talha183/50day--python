06d1f8326a0efa93aa6e5abbb83e502b87fd9eda
Muhammad361738 <ullahehsan016@gmail.com>
2024-12-12 00:48:30 +0500

    Add new file of data types in python joureny

---
+++ python_neduet/basic_operation.py
@@ -0,0 +1,21 @@
+a = 15
+b = 10
+
+addition = a + b #adding both integers
+multiplication = a * b #multiplying both integers
+subtraction = a - b #subtracting two integers
+division = a / b #dividing two integers
+power = a ** 2 #calculating power of 15^2
+floor_division = a//b #returns quotient (integer not float)
+modulus = a % b #returns remainder if any.
+#displaying results
+print(addition) #answer : 25
+print(subtraction) #answer : 5
+print(multiplication) #answer : 150
+print(division) #answer : 1.5
+print(power) #answer : 225
+print(floor_division) #answer: 1
+print(modulus) #answer : 5
+
+a = float(input('Enter a number: '))
+b = int(input('Enter another number: '))
