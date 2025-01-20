f0452bdb9db93c8eca2aa63db2f18fc563fc0fdc
Muhammad361738 <ullahehsan016@gmail.com>
2025-01-20 23:39:09 +0500

    add new file

---
+++ python_neduet/polymorphish.py
@@ -0,0 +1,464 @@
+Skip to main content
+Google Classroom
+Classroom
+Batch 2: Python Development
+Group 2
+Stream
+Classwork
+People
+Stream
+Batch 2: Python Development
+Group 2
+Upcoming
+Woohoo, no work due in soon!
+
+Announce something to your class
+
+Post by Khizar Khan
+Khizar Khan
+Created 12 Jan12 Jan
+Respected teacher, please let us know if you have received the pictures and videos.
+1 class comment
+
+Areeba Jamshed12 Jan
+I received JazakAllah 😊
+
+Add class comment…
+
+
+Announcement: 'Kindly share Group Photos with me via…'
+Areeba Jamshed
+Created 11 Jan11 Jan
+Kindly share Group Photos with me via Email everyone.
+1 class comment
+
+Khizar Khan11 Jan
+My respected teacher, kindly check your inbox.
+
+Add class comment…
+
+
+Announcement: 'Thank you so much everyone for the…'
+Areeba Jamshed
+Created 10 Jan10 Jan
+Thank you so much everyone for the beautiful token of appreciation, It was very very kind and sensible of all of you those who contributed and those who remained consistent throughout this 2 month long journey and didn't give up.
+
+I loved every word in the note you gave me, loved every one of yours gesture.
+
+It made me delighted how all of you respectfully obeyed my advices and guidelines every single time.
+
+I wish you all the very best for your endeavors ahead. Remember me in your prayers. And I am always here for help anytime.
+
+If you stuck somewhere or need advice, i am all ears.
+
+JazakAllah. And May Allah Bless You all with Success, Humbleness, Gratitude, Kindness and Happiness in Life. ❤Aameen.
+P.S: Kindly Share Pictures With Me :)
+Regards,
+Areeba Jamshed.
+
+Khizar Khan12 Jan
+INSHALLAH 💖
+
+Add class comment…
+
+
+Announcement: 'Attention Students: The paper will be…'
+Areeba Jamshed
+Created 10 Jan10 Jan
+Attention Students:
+The paper will be started at 1:40. And End at 3:10.
+Be on time.
+1 class comment
+
+Khizar Khan10 Jan
+👍
+
+Add class comment…
+
+
+Announcement: 'Lecture Exception Handling'
+Areeba Jamshed
+Created 10 Jan10 Jan
+Lecture Exception Handling
+
+exceptionhandling.py
+Text
+
+Add class comment…
+
+
+Announcement: 'Everyone Be On Time at Sharp 12:00pm'
+Areeba Jamshed
+Created 10 Jan10 Jan
+Everyone Be On Time at Sharp 12:00pm
+
+Add class comment…
+
+
+Announcement: 'Practice Questions'
+Areeba Jamshed
+Created 9 Jan9 Jan
+Practice Questions
+
+Practice%20Problems%20for%20Finals.docx
+Word
+1 class comment
+
+shamsa khan9 Jan
+Miss paper ki timing bata dein
+
+Add class comment…
+
+
+Announcement: 'Remaining Lecture Codes'
+Areeba Jamshed
+Created 5 Jan5 Jan
+Remaining Lecture Codes
+
+abstraction.py
+Text
+
+access_modifiers.py
+Text
+
+association.py
+Text
+
+multipleinheretence.py
+Text
+
+polymorphism.py
+Text
+
+Add class comment…
+
+
+Announcement: 'OOP Assignment Submission Reminder!!'
+Areeba Jamshed
+Created 3 Jan3 Jan
+OOP Assignment Submission Reminder!!
+
+Add class comment…
+
+
+Announcement: 'All students are requested to obtain an…'
+Areeba Jamshed
+Created 2 Jan2 Jan
+All students are requested to obtain an affidavit (a requirement for the certificate & to appear in the finals) from Ali Photostat, located behind the Abul Kalam Library or infront of Masijid . Please fill it out and submit it there directly. The cost of the affidavit is Rs. 130. Make sure to complete this process by Friday.
+
+Add class comment…
+
+
+Announcement: 'OOP Lecture Slides'
+Areeba Jamshed
+Created 2 Jan2 Jan
+OOP Lecture Slides
+
+OOP Lectures.pdf
+PDF
+
+Add class comment…
+
+
+Announcement: 'Attention Students: Be on time…'
+Areeba Jamshed
+Created 31 Dec 202431 Dec 2024 (Edited 31 Dec 2024)
+Attention Students:
+
+Be on time tomorrow, also don't get absent in any case. If class is meant to be held, it will be held no matter what.
+and you have to show your presence.
+
+In case, if anyone got absent tomorrow and any day onwards he/she won't be allowed to give final paper.
+
+Hope, you will not disappoint with your class strength next time.
+
+Regards,
+Areeba Jamshed.
+
+Add class comment…
+
+
+Announcement: 'I have updated assignment submission…'
+Areeba Jamshed
+Created 30 Dec 202430 Dec 2024
+I have updated assignment submission deadline till 3rd Jan, as per your request to need little more time to complete it.
+
+Add class comment…
+
+
+Announcement: 'A/S Everyone! Kindly Pay Attention to…'
+Areeba Jamshed
+Created 28 Dec 202428 Dec 2024
+A/S Everyone! Kindly Pay Attention to Reading it:
+I am writing it with an aim to encourage you all in our Programming journey uptil yet. I have felt most of you have lost momentum right after mid term and as soon as we started OOPs.
+
+If for some reasons you think you couldn't do well in Mid Term, no worries let's move forward. Becasue the one who tries never Fails. So let's try again together. 
+
+Just wanted to ensure all sort of guidance and assistance in understanding OOPs from our side, and we can have it sorted by giving a little more attention to concepts by re-understanding those once again.
+
+Just Do The Following And Your Chances to get Certified as Python Developer will be marginally increased:
+
+1) Remain Regular From Monday (30th Dec) onward to get certified. It is just 8-9 day long comittment. 
+2) I'll be covering all OOPs this week, and will be helping you all in understanding old and new concepts individually.
+3) From Monday we'll be actually practicing for finals, to get a good score in it, so all of you those who were regular before mid term, kindly do attend classes and Don't Give Up at this Point.. 
+
+Do discuss your queries and concerns with me, if your are unable to understand any topic/ concept/ logic we can have it sorted. 
+
+Lastly, if you can aim it, you can do it!!
+
+Regrads, 
+Areeba Jamshed.
+
+Muhammad Talha30 Dec 2024
+"What's the update for tomorrow's class? There is a protest going on in the city, especially on University Road."
+
+Add class comment…
+
+
+Announcement: 'Attention Students, Kindly submit your…'
+Areeba Jamshed
+Created 27 Dec 202427 Dec 2024
+Attention Students,
+Kindly submit your assignments by Monday 30th Dec. No deadline extension is possible.
+Moreover, submit your files in (yourname.ipynb) manner.
+
+Add class comment…
+
+
+Announcement: 'i can reconduct mid term of only…'
+Areeba Jamshed
+Created 27 Dec 202427 Dec 2024
+i can reconduct mid term of only absentees, so those who couldn't give midterm due to some reasons they can give it This Week.
+Kindly write your names below those who are interested (only those who didn't give exam earlier).
+Kindly write it down to me by Sunday.
+
+regard,
+Areeba jamshed
+
+Add class comment…
+
+Assignment: "OOPs Assignment"
+Areeba Jamshed posted a new assignment: OOPs Assignment
+Created 27 Dec 202427 Dec 2024 (Edited 30 Dec 2024)
+
+Announcement: 'Remaining Lectures' Code Files'
+Areeba Jamshed
+Created 25 Dec 202425 Dec 2024
+Remaining Lectures' Code Files
+
+access_modifiers.py
+Text
+
+class_method.py
+Text
+
+inheritence.py
+Text
+
+instanceClass_var.py
+Text
+
+static_method.py
+Text
+
+Add class comment…
+
+
+Announcement: 'Revise all OOPs concepts today, I'll…'
+Areeba Jamshed
+Created 25 Dec 202425 Dec 2024 (Edited 25 Dec 2024)
+Revise all OOPs concepts today, I'll give a problem/ short project to make in tomorrow's class.
+Important Python Concepts Revision in 20 mins: https://youtu.be/Gx5qb1uHss4?si=TDJ34dB1piAgK9C9
+OOPs Revision in 20 mins: https://youtu.be/rLyYb7BFgQI?si=-jKqm_TMX1l7tCa4
+Must Go Through These and all Provided files, this is supposed to be your homework for today.
+
+Areeba Jamshed25 Dec 2024
+Already shared in this post. Find it attached
+
+Add class comment…
+
+
+Announcement: 'Dear Students, Please be informed that…'
+Areeba Jamshed
+Created 23 Dec 202423 Dec 2024
+Dear Students,
+Please be informed that there will be no classes on 25th and 27th December. However, the class on 26th December will be held as per schedule.
+
+Add class comment…
+
+"""Polymorphism: The existance of an 
+object into different forms
+- Polymorphism is a concept in OOP but
+ has no discrete syntax, unlike Inheritence.
+
+Concepts in Polymorphism: 
+1) Method Overriding (as discussed in inheritence)
+2) Method Overloadig
+3) Operator Overloading
+"""
+
+#Understanding Method Overriding
+# class Parent:
+#     def __init__(self, age , name):
+#         self.age = age
+#         self.name =name
+#     def show(self):
+#         print('I am a parent class')
+# class Child (Parent):
+#     def __init__(self, age, name, sal):
+#         super().__init__(age, name)
+#         self.sal = sal
+#     def showinfo(self):
+#         # super().showinfo()
+#         print('I am a child class')
+
+# obj = Child(10,  'abc', 3000)
+# obj.showinfo()
+# obj.show() #output: i am child class
+# # obj.showinfo()
+
+"""Method Overloading: Python doesn't support 
+direct method (function) overlaoding
+i.e If we use a function/ method with 
+same name twice or more times in a class
+it is known as method(function) overloading.
+
+How to treat method overloading?
+    1) By default arguments
+    2) By VLA (variable length arguments) *args 
+"""
+# #Understanding Method Overloading (extended functionality)
+# #overwriting + overloading
+# class A:
+#     def addition (self, a , b):  #overwritten by latest method
+#         return a + b
+#     def addition (self, a , b, c):  
+#          return a + b + c
+# #method is overloading with multioperands functionality
+
+# obj = A()
+# # print(obj.addition (2 , 3))
+# print(obj.addition(2 ,3 ,4))
+
+#overload only without overwriting it
+#Treating overwriting with VLA (arbitrary pos arg)
+# class A :
+#     def addition (self, *args):
+#         sum = 0
+#         for i in args:
+#             sum = sum + i 
+#         return sum
+# obj = A ()
+# #now it is working on variable lenght argument
+# print(obj.addition(1,2,3,5,7))
+# print(obj.addition(22, 33, 44, 2, 4, 9, 80, 0, 5, 6))
+
+
+# class A: 
+#     def addition (self, a , b, c = 0):
+#         return a + b + c
+# obj = A()
+# print(obj.addition(1 , 2))
+
+"""Operator Overlaoding: 
+Operators: Arithmetic ( +, - , * , / etc)
+Comparison(<, >, ==), Bitwise (and, or etc) 
+If some operator is overloaded beyond
+its operations or capabilities is operator overloading.
+How to treat it? 
+    1) using dunder method (magic dunder method) 
+    inside class, exclusively
+    for the certain operator
+"""
+
+# #functionality of add (operator is extended here)
+# print(2 + 3)  #used as sum
+# print('2'+ '9') #used in concatenation
+# print(int.__add__(1 ,2))
+# print(str.__add__('A','B'))
+
+"""
+class A:
+    pass
+obj1 = A()
+obj2 = A()
+print(obj1 + obj2) == ??
+
+"""
+"""obj1 = 2 + 3i
+    obj2 = 1 + 4i
+    print(obj1+obj2)= 3 + 7i"""
+
+# print(int.__add__(2 , 3))
+# print(2 + 3)
+class ComplexNum :
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+    def __add__(obj1 , obj2):
+        return f'{obj1.real + obj2.real} + {obj1.imag + obj2.imag}i'
+obj1 = ComplexNum(1 , 2)
+obj2 = ComplexNum(2 , 3)
+print(obj1 + obj2)  #3 + 5i
+
+
+'''
+
+Qs: Illustrate operator overloading on vector addition
+vector 1 ( 2 , 3, 5) ==> 2i + 3j + 5k
+vector 2 ( 1 , 4, 7) ==> i + 4j + 7k
+(vector1 + vector2) ===> (2 + 1)i + (3 + 4)j + (5 + 7)k
+
+
+Qs: Make employee eligible for bonus, if his salary is less.
+obj1 = employee(sal=6666)  (make dunder method using __gt__/__lt__)
+obj2 = employee(sal===8888)
+if
+    obj1>obj
+    print(obj2 is valid for bonus)
+else:
+    print(obj1 is valid for bonus)
+
+'''
+class Dog:
+    def sound(self):
+        return "Bark"
+
+class Cat:
+    def sound(self):
+        return "Meow"
+class Animal:
+    def sound (self):
+        return 'ABC'
+
+d = Dog()
+c = Cat()
+e = Animal()
+print(e.sound())
+print(d.sound())
+print(c.sound())
+
+
+##created interface 
+animal = [d , c, e]
+animal = [Dog(), Cat(), Animal()]
+
+for i in animal:
+    print(i.sound())
+
+# class Dog:
+#     def sound(self):
+#         return "Bark"
+
+# class Cat:
+#     def sound(self):
+#         return "Meow"
+
+# def test (animal):
+#     print(animal.sound())
+
+# animal1 = Dog()
+# animal2 = Cat()
+
+# test(animal1)
+# test(animal2)
